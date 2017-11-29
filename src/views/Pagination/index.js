96690193aa3456c85544f29eb99a55d17b7ae7d2
Aliaksei_Rachanok <bagarottiz@gmail.com>
2017-11-29 14:13:18 +0300

    Pagination component added

--- src/views/Pagination/index.js
+++ src/views/Pagination/index.js
@@ -51,7 +51,7 @@ class Pagination extends Component {
         let pagination = pageLinks.length !== 1 ? <div className="pagination">{pageLinks}</div> : ' ';
 
         return (
-            <div>
+            <div className='someChanges'>
                 {pagination}
             </div>
         );
